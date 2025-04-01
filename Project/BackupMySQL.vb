fd0652fbb9329d11bd68182118e6e4049adfb315
Guilherme <guilhontina@hotmail.com>
2025-04-01 09:33:02 -0300

    ok

---
+++ Project/BackupMySQL.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Diagnostics
+Imports System.IO
+Imports System.IO.Compression
+
+Public Class BackupMySQL
+    Public Shared Function Backup(ByVal caminhoBackup As String, ByVal usuario As String, ByVal senha As String, ByVal banco As String, ByVal caminhoMysqlDump As String) As Boolean
+        Try
+            Dim processo As New Process()
+            processo.StartInfo.FileName = caminhoMysqlDump
+            processo.StartInfo.Arguments = $"--user={usuario} --password={senha} --host=localhost {banco} --result-file=""{caminhoBackup}"""
+            processo.StartInfo.RedirectStandardOutput = True
+            processo.StartInfo.RedirectStandardError = True
+            processo.StartInfo.UseShellExecute = False
+            processo.StartInfo.CreateNoWindow = True
+            processo.Start()
+
+            processo.WaitForExit()
+            If processo.ExitCode = 0 Then
+                ' Compactar o backup após a criação
+                Dim caminhoZip As String = CompactarBackup(caminhoBackup)
+                Return Not String.IsNullOrEmpty(caminhoZip)
+            Else
+                Return False
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Erro ao criar backup: " & ex.Message)
+            Return False
+        End Try
+    End Function
+
+    Public Shared Function CompactarBackup(ByVal caminhoBackup As String) As String
+        Try
+            Dim caminhoZip As String = Path.ChangeExtension(caminhoBackup, ".zip")
+            Using zip As FileStream = New FileStream(caminhoZip, FileMode.Create)
+                Using arquivoZip As ZipArchive = New ZipArchive(zip, ZipArchiveMode.Create)
+                    arquivoZip.CreateEntryFromFile(caminhoBackup, Path.GetFileName(caminhoBackup), CompressionLevel.Optimal)
+                End Using
+            End Using
+            File.Delete(caminhoBackup) ' Remove o arquivo original após a compactação
+            Return caminhoZip
+        Catch ex As Exception
+            MessageBox.Show("Erro ao compactar backup: " & ex.Message)
+            Return String.Empty
+        End Try
+    End Function
+End Class
---
+++ Project/GoogleDriveUploader.vb
@@ -0,0 +1,37 @@
+﻿Imports Google.Apis.Auth.OAuth2
+Imports Google.Apis.Drive.v3
+Imports Google.Apis.Services
+Imports Google.Apis.Upload
+Imports System.IO
+
+Public Class GoogleDriveUploader
+
+    Public Shared Function UploadArquivo(ByVal caminhoArquivo As String, ByVal credenciaisPath As String) As String
+        Try
+            Dim credential As GoogleCredential
+            Using stream = New FileStream(credenciaisPath, FileMode.Open, FileAccess.Read)
+                credential = GoogleCredential.FromStream(stream).CreateScoped(DriveService.ScopeConstants.DriveFile)
+            End Using
+
+            Dim service = New DriveService(New BaseClientService.Initializer() With {
+                .HttpClientInitializer = credential,
+                .ApplicationName = "MysqlBackupSecretaria"
+            })
+
+            Dim fileMetadata = New Google.Apis.Drive.v3.Data.File() With {
+                .Name = Path.GetFileName(caminhoArquivo)
+            }
+
+            Using fileStream = New FileStream(caminhoArquivo, FileMode.Open)
+                Dim request = service.Files.Create(fileMetadata, fileStream, "application/octet-stream")
+                request.Fields = "id"
+                request.Upload()
+                Return request.ResponseBody.Id
+            End Using
+        Catch ex As Exception
+            MessageBox.Show("Erro ao fazer upload para o Google Drive: " & ex.Message)
+            Return String.Empty
+        End Try
+    End Function
+
+End Class
