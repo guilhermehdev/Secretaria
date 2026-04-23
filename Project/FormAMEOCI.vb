4f13657571056f32156ab743a8bde77c4121636f
Guilherme <guilhontina@hotmail.com>
2026-04-23 08:58:54 -0300

    ok

--- Project/FormAMEOCI.vb
+++ Project/FormAMEOCI.vb
@@ -327,8 +327,8 @@ Public Class FormAMEOCI
                 TabControl1.SelectedTab = TabControl1.TabPages(0)
             End If
 
-            If txtProcedimentoPrincipal.SelectedValue = "0902010026" AndAlso CInt(m.AgeInMonths(m.mysqlDateFormat(dtNascimento.Text), m.mysqlDateFormat(dtValidadeIni.Value))) < 144 Then
-                Throw New Exception("Paciente com idade inferior a 12 anos não permitido para procedimento 0902010026.")
+            If txtProcedimentoPrincipal.SelectedValue = "0902010026" AndAlso CInt(m.AgeInMonths(m.mysqlDateFormat(dtNascimento.Text), m.mysqlDateFormat(dtValidadeIni.Value))) < 216 Then
+                Throw New Exception("Paciente com idade inferior a 18 anos não permitido para procedimento 0902010026.")
                 TabControl1.SelectedTab = TabControl1.TabPages(0)
             End If
 
